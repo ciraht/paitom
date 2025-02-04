68b8be73fe7ce54adad4b9ab6cc7c92ec35a36ee
ciraht <167528563+ciraht@users.noreply.github.com>
2025-02-04 17:20:36 -0300

    Add files via upload

--- view.py
+++ view.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from main import app, con
 
 
@@ -15,4 +15,64 @@ def livro():
             'autor': livro[2],
             'ano_publicacao': livro[3]
         })
-        return jsonify(mensagem='Lista de Livros', livros=livros_dic)
+    return jsonify(mensagem='Lista de Livros', livros=livros_dic)
+
+
+@app.route('/livro', methods=['POST'])
+def livro_post():
+    data = request.get_json()
+    titulo = data.get('titulo')
+    autor = data.get('autor')
+    ano_publicacao = data.get('ano_publicacao')
+
+    cursor = con.cursor()
+
+    cursor.execute('SELECT 1 FROM LIVROS WHERE TITULO = ?', (titulo,))
+
+    if cursor.fetchone():
+        return jsonify("Livro já cadastrado")
+
+    cursor.execute("INSERT INTO LIVROS(TITULO, AUTOR, ANO_PUBLICACAO) VALUES (?, ?, ?)",
+                   (titulo, autor, ano_publicacao))
+
+    con.commit()
+    cursor.close()
+
+    return jsonify({
+        'mensagem': "Livro cadastrado com sucesso!",
+        'livro': {
+            'titulo': titulo,
+            'autor': autor,
+            'ano_publicacao': ano_publicacao
+        }
+    })
+
+@app.route('/livro/<int:id>', methods=['PUT'])
+def livro_put(id):
+    cursor = con.cursor()
+    cursor.execute("SELECT ID_LIVRO, TITULO, AUTOR, ANO_PUBLICACAO FROM LIVROS WHERE ID_LIVRO = ?", (id,))
+    livro_data = cursor.fetchone()
+
+    if not livro_data:
+        cursor.close()
+        return jsonify({"mensagem": "Livro não foi encontrado"})
+
+    data = request.get_json()
+    titulo = data.get('titulo')
+    autor = data.get('autor')
+    ano_publicacao = data.get('ano_publicacao')
+
+    cursor.execute("UPDATE LIVROS SET TITULO = ?, AUTOR = ?, ANO_PUBLICACAO = ? WHERE ID_LIVRO = ?",
+                   (titulo, autor, ano_publicacao, id))
+    con.commit()
+    cursor.close()
+
+    return jsonify({
+        'mensagem': "Livro editado com sucesso!",
+        'livro': {
+            'id_livro': id,
+            'titulo': titulo,
+            'autor': autor,
+            'ano_publicacao': ano_publicacao
+        }
+    })
